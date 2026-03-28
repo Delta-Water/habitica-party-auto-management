f1d3243183659847f2d8f626fda617dd21234399
Delta-Water <3627371741@qq.com>
2026-03-28 16:32:10 +0800

    Change Python command to use python3

--- start.py
+++ start.py
@@ -6,7 +6,7 @@ scripts = ["./scripts/manage_members.py", "./scripts/update_description.py"]
 for script in scripts:
     print(f"正在执行: {script}")
     # 使用 os.system 执行脚本
-    exit_code = os.system(f"python {script}")
+    exit_code = os.system(f"python3 {script}")
     
     # 检查执行结果
     if exit_code != 0:
